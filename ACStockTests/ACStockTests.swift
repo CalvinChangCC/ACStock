e31f54d3013d4c8cc9642c67a870aed7b948e252
Calvin Chang <calvin.chang.taiwan@gmail.com>
2020-04-27 11:46:13 +0200

    [Setup] Project init

---
+++ ACStockTests/ACStockTests.swift
@@ -0,0 +1,34 @@
+//
+//  ACStockTests.swift
+//  ACStockTests
+//
+//  Created by Chang Wen-Lung on 27.04.20.
+//  Copyright © 2020 Accelgor. All rights reserved.
+//
+
+import XCTest
+@testable import ACStock
+
+class ACStockTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+inhibit_all_warnings!
+
+target 'ACStock' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Swinject'
+  pod 'SwiftLint'
+
+  target 'ACStockTests' do
+    inherit! :search_paths
+
+    pod 'Quick'
+    pod 'Nimble'
+    pod 'Fakery'
+  end
+
+end
